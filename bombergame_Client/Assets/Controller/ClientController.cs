c008208b6b2c8cb56a116e4eecf68004ad8ad108
DESKTOP-VDRQSDJ\User <nutsineethgmail.com>
2021-09-26 11:25:50 +0700

    Add Server

---
+++ bombergame_Client/Assets/Controller/ClientController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ClientController : MonoBehaviour
+{
+    [SerializeField] private Client client;
+}
---
+++ bombergame_Client/Assets/Scripts/ModelToObjectMapper.cs
@@ -0,0 +1,33 @@
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelToObjectMapper
+{
+    private Dictionary<string, Action<JObject>> Deserializes;
+
+    private readonly ClientController clientController;
+
+    private Dictionary<string, Action<JObject>> CreateDeserializes() => new Dictionary<string, Action<JObject>>
+    {
+        //TODO
+    };
+
+    public ModelToObjectMapper(ClientController clientController)
+    {
+        Deserializes = CreateDeserializes();
+        this.clientController = clientController;
+    }
+
+    public void DeserializeToFunction(string json)
+    {
+        var jObject = JObject.Parse(json);
+        if (jObject.TryGetValue("ClassName", out var value))
+        {
+            var className = value.ToString();
+            Deserializes[className](jObject);
+        }
+    }
+
+}
